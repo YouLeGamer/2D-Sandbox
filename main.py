988b195bebd96e95c19897e2e2363bb5ca192a19
YouLeGamer <yoann.vandomme@outlook.fr>
2023-06-15 21:09:55 +0200

    Added several files
    - Added Random Things module (aka UselessUtilities)
    - Added main.py, with game loop, window name and snapshot name, screen creation and imports
    - Updated .gitignore (added *.pyc)

---
+++ main.py
@@ -0,0 +1,24 @@
+import scripts.random_things as rt
+import pygame
+import time
+import os
+
+# Init pygame
+pygame.init()
+
+game_version = "- Classic 23w14"
+
+screen_size = (800, 600)
+screen_color = (0, 0, 0)
+screen = pygame.display.set_mode(screen_size)
+
+pygame.display.set_caption("2D Sandbox Game " + game_version)
+
+# GAME LOOP -----------------------------------------------------------------------------------------
+
+running = True
+while running:
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
---
+++ scripts/random_things.py
@@ -0,0 +1,167 @@
+from time import sleep
+import os
+
+"""
+Welcome in the Random Things Functions Module!
+
+Here are the list of all the functions, and of what they do:
+
+max() -> Takes two integers and returns the biggest.
+getVowel() -> Counts the vowels of a specified string and returns the number.
+getCons() -> Counts the consonants of a specified string and returns the number.
+convertBinary() -> Converts an integer in binary (does the same as bin())
+typewriter() -> Prints a string in a "typewriter" way.
+typewriterList() -> Variant of typewriter() for lists, prints each value in a "typewriter" way.
+firstRemIdentity() -> Makes the first remarkable identity's equation.
+secondRemIdentity() -> Makes the second remarkable identity's equation.
+thirdRemIdentity() -> Makes the third remarkable identity's equation.
+ifExist() -> Returns true if the path exists. (Batch's "if exist")
+ifNotExist() -> Returns true if the path doesn't exists. (Batch's "if not exist")
+isOdd() -> Returns true if the number is odd.
+isEven() -> Returns true if the number is even.
+"""
+
+
+def max(a, b):
+    """
+    Gets two integers and sends back the biggest.
+    """
+    if a > b:
+        return a
+    else:
+        return b
+
+
+def getVowel(word, y_vowel=True):
+    """
+    Takes a string as a parameter and sends back the number of vowels.
+    The parameter "y_vowel" allows the user to set Y as a vowel. By default, it's on True.
+    """
+    vowel_count = 0
+    if y_vowel == True:
+        vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
+    else:
+        vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    for letter in word:
+        if letter in vowels:
+            vowel_count += 1
+        continue
+    return vowel_count
+
+
+def getCons(word, y_consonant=False):
+    """
+    Takes a word as a parameter and sends back the number of consonants.
+    The parameter "y_consonant" allows the user to set Y as a consonant. By default, it's on False.
+    """
+    cons_count = 0
+    if y_consonant == False:
+        consonants = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w",
+                      "x", "z", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S",
+                      "T", "V", "W", "X", "Z"]
+    else:
+        consonants = ["b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w",
+                      "x", "y", "z", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "M", "N", "P", "Q", "R", "S",
+                      "T", "V", "W", "X", "Y", "Z"]
+    for letter in word:
+        if letter in consonants:
+            cons_count += 1
+        continue
+    return cons_count
+
+
+def convertBinary(n=1):
+    """
+    Converts a number in binary. By default, it's 1.
+    """
+    return bin(n)
+
+
+def typewriter(string):
+    """
+    Prints a string in a typewriter style.
+    """
+    for char in string:
+        sleep(0.05)
+        print(char, end='')
+    print("\n")
+
+
+def typewriterList(list):
+    """
+    Prints a list of values in a single line in a typewriter style.
+    """
+    for value in list:
+        sleep(0.1)
+        print(value + " ")
+    print("\n")
+
+
+def firstRemIdentity(a, b):
+    """
+    Makes the first remarkable identity's equation:
+        (a+b)² = a² + 2ab + b²
+    and returns the result. Takes a and b as parameters.
+    """
+    result = a ** 2 + 2 * (a * b) + b ** 2
+    return result
+
+
+def secondRemIdentity(a, b):
+    """
+    Makes the second remarkable identity's equation:
+        (a-b)² = a² - 2ab + b²
+    and returns the result. Takes a and b as parameters
+    """
+    result = a ** 2 - 2 * (a * b) + b ** 2
+    return result
+
+
+def thirdRemIdentity(a, b):
+    """
+        Makes the third remarkable identity's equation:
+            (a+b)(a-b) = a² - b²
+        and returns the result. Takes a and b as parameters
+    """
+    result = a ** 2 - b ** 2
+    return result
+
+
+def ifNotExist(path):
+    """
+    Returns true if the path doesn't exist.
+    """
+    if os.path.exists(path):
+        return False
+    else:
+        return True
+
+
+def ifExist(path):
+    """
+    Returns true if the path exists.
+    """
+    if os.path.exists(path):
+        return True
+    else:
+        return False
+
+
+def isOdd(number):
+    """
+    Returns true if the number is odd.
+    """
+    if (number % 2) == 0:
+        return False
+    else:
+        return True
+
+
+def isEven(number):
+    """
+    Returns true of the number is even.
+    """
+    if (number % 2) == 0:
+        return True
+    else:
+        return False
